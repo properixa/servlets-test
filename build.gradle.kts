3b37e8057a149724cc548d2508092f813ae101f6
properixa <mpinzhura29@gmail.com>
2026-04-16 18:24:39 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+    id("war")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.war {
+    archiveFileName.set("firstServlet.war")
+}
