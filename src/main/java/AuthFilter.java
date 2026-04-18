41300de6cb47cc6550f6e5fa569b5e726e7a55f5
properixa <mpinzhura29@gmail.com>
2026-04-18 18:22:29 +0300

    Added auth and login filter

--- src/main/java/AuthFilter.java
+++ src/main/java/AuthFilter.java
@@ -1,2 +1,30 @@
-public class AuthFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        String path = req.getRequestURI();
+        if (path.contains("/login")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        HttpSession session = req.getSession(false);
+        if (session != null && session.getAttribute("username") != null) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        resp.sendRedirect("/firstServlet/login");
+    }
 }
--- src/main/java/LoginFilter.java
+++ src/main/java/LoginFilter.java
@@ -1,2 +1,25 @@
-public class LoginFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter("/login")
+public class LoginFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+
+        HttpSession session = req.getSession(false);
+        if (session != null && session.getAttribute("username") != null) {
+            resp.sendRedirect("/firstServlet/welcome");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
 }
